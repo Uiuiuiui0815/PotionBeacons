83c81fa23a109a726464a912ca1fa754a210e670
Uiuiuiui0815 <frank.blaubeerstampfer@gmail.com>
2024-10-19 15:07:58 +0200

    Small changes in code

--- src/main/java/net/uiuiuiui0815/potionbeacons/PotionBeaconBlockEntityRenderer.java
+++ src/main/java/net/uiuiuiui0815/potionbeacons/PotionBeaconBlockEntityRenderer.java
@@ -50,43 +50,37 @@ public class PotionBeaconBlockEntityRenderer implements BlockEntityRenderer<Poti
         float h = MathHelper.fractionalPart(g * 0.2F - (float)MathHelper.floor(g * 0.1F));
         matrices.push();
         matrices.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(f * 2.25F - 45.0F));
-        float j = 0.0F;
-        float m = 0.0F;
+        float j;
+        float m;
         float n = -innerRadius;
-        float o = 0.0F;
-        float p = 0.0F;
         float q = -innerRadius;
-        float r = 0.0F;
-        float s = 1.0F;
         float t = -1.0F + h;
         float u = (float)maxY * heightScale * (0.5F / innerRadius) + t;
-        renderBeamLayer(matrices, vertexConsumers.getBuffer(RenderLayer.getBeaconBeam(textureId, false)), color, yOffset, i, 0.0F, innerRadius, innerRadius, 0.0F, n, 0.0F, 0.0F, q, 0.0F, 1.0F, u, t);
+        renderBeamLayer(matrices, vertexConsumers.getBuffer(RenderLayer.getBeaconBeam(textureId, false)), color, yOffset, i, 0.0F, innerRadius, innerRadius, 0.0F, n, 0.0F, 0.0F, q, u, t);
         matrices.pop();
         j = -outerRadius;
         float k = -outerRadius;
         m = -outerRadius;
         n = -outerRadius;
-        r = 0.0F;
-        s = 1.0F;
         t = -1.0F + h;
         u = (float)maxY * heightScale + t;
-        renderBeamLayer(matrices, vertexConsumers.getBuffer(RenderLayer.getBeaconBeam(textureId, true)), Argb.withAlpha(32, color), yOffset, i, j, k, outerRadius, m, n, outerRadius, outerRadius, outerRadius, 0.0F, 1.0F, u, t);
+        renderBeamLayer(matrices, vertexConsumers.getBuffer(RenderLayer.getBeaconBeam(textureId, true)), Argb.withAlpha(32, color), yOffset, i, j, k, outerRadius, m, n, outerRadius, outerRadius, outerRadius, u, t);
         matrices.pop();
     }
 
-    private static void renderBeamLayer(MatrixStack matrices, VertexConsumer vertices, int color, int yOffset, int height, float x1, float z1, float x2, float z2, float x3, float z3, float x4, float z4, float u1, float u2, float v1, float v2) {
+    private static void renderBeamLayer(MatrixStack matrices, VertexConsumer vertices, int color, int yOffset, int height, float x1, float z1, float x2, float z2, float x3, float z3, float x4, float z4, float v1, float v2) {
         MatrixStack.Entry entry = matrices.peek();
-        renderBeamFace(entry, vertices, color, yOffset, height, x1, z1, x2, z2, u1, u2, v1, v2);
-        renderBeamFace(entry, vertices, color, yOffset, height, x4, z4, x3, z3, u1, u2, v1, v2);
-        renderBeamFace(entry, vertices, color, yOffset, height, x2, z2, x4, z4, u1, u2, v1, v2);
-        renderBeamFace(entry, vertices, color, yOffset, height, x3, z3, x1, z1, u1, u2, v1, v2);
+        renderBeamFace(entry, vertices, color, yOffset, height, x1, z1, x2, z2, v1, v2);
+        renderBeamFace(entry, vertices, color, yOffset, height, x4, z4, x3, z3, v1, v2);
+        renderBeamFace(entry, vertices, color, yOffset, height, x2, z2, x4, z4, v1, v2);
+        renderBeamFace(entry, vertices, color, yOffset, height, x3, z3, x1, z1, v1, v2);
     }
 
-    private static void renderBeamFace(MatrixStack.Entry matrix, VertexConsumer vertices, int color, int yOffset, int height, float x1, float z1, float x2, float z2, float u1, float u2, float v1, float v2) {
-        renderBeamVertex(matrix, vertices, color, height, x1, z1, u2, v1);
-        renderBeamVertex(matrix, vertices, color, yOffset, x1, z1, u2, v2);
-        renderBeamVertex(matrix, vertices, color, yOffset, x2, z2, u1, v2);
-        renderBeamVertex(matrix, vertices, color, height, x2, z2, u1, v1);
+    private static void renderBeamFace(MatrixStack.Entry matrix, VertexConsumer vertices, int color, int yOffset, int height, float x1, float z1, float x2, float z2, float v1, float v2) {
+        renderBeamVertex(matrix, vertices, color, height, x1, z1, (float) 1.0, v1);
+        renderBeamVertex(matrix, vertices, color, yOffset, x1, z1, (float) 1.0, v2);
+        renderBeamVertex(matrix, vertices, color, yOffset, x2, z2, (float) 0.0, v2);
+        renderBeamVertex(matrix, vertices, color, height, x2, z2, (float) 0.0, v1);
     }
 
     private static void renderBeamVertex(MatrixStack.Entry matrix, VertexConsumer vertices, int color, int y, float x, float z, float u, float v) {
@@ -102,6 +96,6 @@ public class PotionBeaconBlockEntityRenderer implements BlockEntityRenderer<Poti
     }
 
     public boolean isInRenderDistance(PotionBeaconEntity beaconBlockEntity, Vec3d vec3d) {
-        return Vec3d.ofCenter(beaconBlockEntity.getPos()).multiply(1.0, 0.0, 1.0).isInRange(vec3d.multiply(1.0, 0.0, 1.0), (double)this.getRenderDistance());
+        return Vec3d.ofCenter(beaconBlockEntity.getPos()).multiply(1.0, 0.0, 1.0).isInRange(vec3d.multiply(1.0, 0.0, 1.0), this.getRenderDistance());
     }
 }
--- src/main/java/net/uiuiuiui0815/potionbeacons/PotionBeaconsClient.java
+++ src/main/java/net/uiuiuiui0815/potionbeacons/PotionBeaconsClient.java
@@ -4,14 +4,14 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
-import net.fabricmc.fabric.impl.client.rendering.BlockEntityRendererRegistryImpl;
 import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.block.entity.BlockEntityRendererFactories;
 
 @Environment(EnvType.CLIENT)
 public class PotionBeaconsClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         BlockRenderLayerMap.INSTANCE.putBlock(PotionBeaconBlock.POTION_BEACON_BLOCK, RenderLayer.getCutout());
-        BlockEntityRendererRegistryImpl.register(PotionBeacons.POTION_BEACON_ENTITY, PotionBeaconBlockEntityRenderer::new);
+        BlockEntityRendererFactories.register(PotionBeacons.POTION_BEACON_ENTITY, PotionBeaconBlockEntityRenderer::new);
     }
 }
